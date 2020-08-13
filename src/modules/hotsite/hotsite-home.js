753a6ede8d543b6d56833d8b6dd5b8a30ec6ddb3
Carlos Costa <carllos.nc@gmail.com>
2020-08-13 12:58:05 -0300

    Updating home page.

--- src/modules/hotsite/hotsite-home.js
+++ src/modules/hotsite/hotsite-home.js
@@ -1,10 +1,38 @@
 import React from "react"
-import { Main } from "../../components"
+import { Main, Link } from "../../components"
+import styled from "styled-components"
+import { IMAGES } from "../../values"
+
+const Center = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  padding: 20px;
+`
+
+const Logo = styled.img`
+  width: 100%;
+  max-width: 300px;
+  margin-bottom: 20px;
+`
+
+const Description = styled.span`
+  font-size: 1em;
+`
 
 export function HotsiteHome () {
   return (
     <Main>
-      <h1> Hotsite </h1>
+      <Center>
+        <Logo src={IMAGES.DECK_LOGO_DARK} />
+        <Description>
+          React dashkit •
+          <Link to="doc/home"> Doc </Link>
+        </Description>
+      </Center>
     </Main>
   )
 }
