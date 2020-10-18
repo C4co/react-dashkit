f8589f5623eef1098887a5102cfafd2e360292e6
Carlos Costa <carllos.nc@gmail.com>
2020-10-18 15:03:58 -0300

    Adding button tests cases.

--- src/components/button/button.test.js
+++ src/components/button/button.test.js
@@ -1,7 +1,7 @@
 import "@testing-library/jest-dom/extend-expect"
 import "@testing-library/jest-dom"
 import "jest-styled-components"
-import { render } from "@testing-library/react"
+import { getByTestId, render } from "@testing-library/react"
 
 import React from "react"
 
@@ -20,7 +20,7 @@ describe("<Button /> component", () => {
     expect(getByTestId("button-wrapper")).toHaveTextContent("button component")
   })
 
-  test("Check button with loading", () => {
+  test("Check loading state", () => {
     const { getByTestId } = render(<Button loading> button component </Button>)
 
     expect(getByTestId("button-spinner")).toBeInTheDocument()
@@ -35,4 +35,37 @@ describe("<Button /> component", () => {
     expect(getByTestId("button-icon-right")).toBeInTheDocument()
   })
 
+  test("Check info state", () => {
+    const { getByTestId } = render(<Button info> button component </Button>)
+
+    expect(getByTestId("button-wrapper")).toHaveStyleRule("background-color", COLORS.INFO)
+    expect(getByTestId("button-wrapper")).toHaveStyleRule("color", COLORS.WHITE)
+  })
+
+  test("Check danger state", () => {
+    const { getByTestId } = render(<Button danger> button component </Button>)
+
+    expect(getByTestId("button-wrapper")).toHaveStyleRule("background-color", COLORS.DANGER)
+    expect(getByTestId("button-wrapper")).toHaveStyleRule("color", COLORS.WHITE)
+  })
+
+  test("Check warn state", () => {
+    const { getByTestId } = render(<Button warn> button component </Button>)
+
+    expect(getByTestId("button-wrapper")).toHaveStyleRule("background-color", COLORS.WARN)
+    expect(getByTestId("button-wrapper")).toHaveStyleRule("color", COLORS.WHITE)
+  })
+
+  test("Check success state", () => {
+    const { getByTestId } = render(<Button success> button component </Button>)
+
+    expect(getByTestId("button-wrapper")).toHaveStyleRule("background-color", COLORS.SUCCESS)
+    expect(getByTestId("button-wrapper")).toHaveStyleRule("color", COLORS.WHITE)
+  })
+
+  test("Check full width style", () => {
+    const { getByTestId } = render(<Button full> button component </Button>)
+
+    expect(getByTestId("button-wrapper")).toHaveStyleRule("width", "100%")
+  })
 })
