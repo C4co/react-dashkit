098fabbcea809594b9afc44210be16b7d0366c22
Carlos Costa <carllos.nc@gmail.com>
2020-11-08 14:26:32 -0300

    Adding new checkbox test cases.

--- src/components/checkbox/checkbox.test.js
+++ src/components/checkbox/checkbox.test.js
@@ -1,7 +1,7 @@
 import "@testing-library/jest-dom/extend-expect"
 import "@testing-library/jest-dom"
 import "jest-styled-components"
-import { render } from "@testing-library/react"
+import { render, fireEvent } from "@testing-library/react"
 
 import React from "react"
 
@@ -23,5 +23,26 @@ describe("<Checkbox /> component", () => {
 
     expect(getByTestId("checkbox-description")).toBeInTheDocument()
     expect(getByTestId("checkbox-description")).toHaveTextContent("checkbox description")
+  })
+
+  test("Check values", () => {
+    let mockValue = null
+
+    const { getByTestId } = render(
+      <Checkbox
+        label="checkbox component"
+        description="checkbox descriptione"
+        value={10}
+        onChange={event => {
+          mockValue = event.target.value
+        }}
+      />
+    )
+
+    expect(getByTestId("checkbox-element")).toHaveAttribute("value", "10")
+
+    fireEvent.click(getByTestId("checkbox-element"))
+
+    expect(mockValue).toBe("10")
   })
 })
