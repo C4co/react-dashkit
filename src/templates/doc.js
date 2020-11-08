e20efa1674a775e442121027f0756b1c7097eb91
Carlos Costa <carllos.nc@gmail.com>
2020-11-08 19:16:02 -0300

    Small refacts.

--- src/templates/doc.js
+++ src/templates/doc.js
@@ -30,7 +30,8 @@ import {
   FaStopCircle,
   FaExclamationTriangle,
   FaSmileBeam,
-  FaCss3
+  FaCss3,
+  FaLock
 } from "react-icons/fa"
 
 const DocContainer = styled.section`
@@ -75,126 +76,126 @@ export function DocTemplate ({ actived, children, title }) {
           <SideMenuSection title="Components">
             <SideMenuItem
               icon={FaHome}
-              actived={actived === "home" ? 1 : 0}
+              actived={actived === "home"}
               to="/doc/home">
               Home
             </SideMenuItem>
 
             <SideMenuItem
               icon={FaFont}
-              actived={actived === "typography" ? 1 : 0}
+              actived={actived === "typography"}
               to="/doc/typography">
               Typography
             </SideMenuItem>
 
             <SideMenuItem
               icon={FaEdit}
-              actived={actived === "inputs" ? 1 : 0}
+              actived={actived === "inputs"}
               to="/doc/inputs">
               Inputs
             </SideMenuItem>
 
             <SideMenuItem
               icon={FaCheckSquare}
-              actived={actived === "check" ? 1 : 0}
+              actived={actived === "check"}
               to="/doc/checkbox">
               Checkbox
             </SideMenuItem>
 
             <SideMenuItem
               icon={FaRegDotCircle}
-              actived={actived === "radio" ? 1 : 0}
+              actived={actived === "radio"}
               to="/doc/radio">
               Radio
             </SideMenuItem>
 
             <SideMenuItem
               icon={FaSquare}
-              actived={actived === "buttons" ? 1 : 0}
+              actived={actived === "buttons"}
               to="/doc/buttons">
               Buttons
             </SideMenuItem>
 
             <SideMenuItem
               icon={FaChevronDown}
-              actived={actived === "select" ? 1 : 0}
+              actived={actived === "select"}
               to="/doc/select">
               Select
             </SideMenuItem>
 
             <SideMenuItem
               icon={FaThLarge}
-              actived={actived === "block" ? 1 : 0}
+              actived={actived === "block"}
               to="/doc/block">
               Block
             </SideMenuItem>
 
             <SideMenuItem
               icon={FaColumns}
-              actived={actived === "sidemenu" ? 1 : 0}
+              actived={actived === "sidemenu"}
               to="/doc/sidemenu">
               Sidemenu
             </SideMenuItem>
 
             <SideMenuItem
               icon={FaCalendarAlt}
-              actived={actived === "datepicker" ? 1 : 0}
+              actived={actived === "datepicker"}
               to="/doc/datepicker">
               Date Picker
             </SideMenuItem>
 
             <SideMenuItem
               icon={FaCode}
-              actived={actived === "syntax" ? 1 : 0}
+              actived={actived === "syntax"}
               to="/doc/syntax">
               Syntax
             </SideMenuItem>
 
             <SideMenuItem
               icon={FaBorderAll}
-              actived={actived === "grid" ? 1 : 0}
+              actived={actived === "grid"}
               to="/doc/grid">
               Grid
             </SideMenuItem>
 
             <SideMenuItem
               icon={FaAlignJustify}
-              actived={actived === "form" ? 1 : 0}
+              actived={actived === "form"}
               to="/doc/form">
               Form
             </SideMenuItem>
 
             <SideMenuItem
               icon={FaSpinner}
-              actived={actived === "spinner" ? 1 : 0}
+              actived={actived === "spinner"}
               to="/doc/spinner">
               Spinner
             </SideMenuItem>
 
             <SideMenuItem
               icon={FaTable}
-              actived={actived === "table" ? 1 : 0}
+              actived={actived === "table"}
               to="/doc/table">
               Table
             </SideMenuItem>
 
             <SideMenuItem
               icon={FaStopCircle}
-              actived={actived === "badge" ? 1 : 0}
+              actived={actived === "badge"}
               to="/doc/badge">
               Badge
             </SideMenuItem>
 
             <SideMenuItem
               icon={FaExclamationTriangle}
-              actived={actived === "alert" ? 1 : 0}
+              actived={actived === "alert"}
               to="/doc/alert">
               Alert
             </SideMenuItem>
 
             <SideMenuItem
               icon={FaSmileBeam}
-              actived={actived === "Avatar" ? 1 : 0}
+              actived={actived === "Avatar"}
               to="/doc/avatar">
               Avatar
             </SideMenuItem>
@@ -203,9 +204,18 @@ export function DocTemplate ({ actived, children, title }) {
           <SideMenuSection title="Helpers">
             <SideMenuItem
               icon={FaCss3}
-              actived={actived === "classes" ? 1 : 0}
+              actived={actived === "classes"}
               to="/doc/classes">
               CSS Classes
+            </SideMenuItem>
+          </SideMenuSection>
+
+          <SideMenuSection title="Pages">
+            <SideMenuItem
+              icon={FaLock}
+              actived={actived === "login"}
+              to="/login">
+              Login page
             </SideMenuItem>
           </SideMenuSection>
 
