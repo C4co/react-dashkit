53cd0de35db75144af18d73b0be7b9573771c6b6
Carlos Costa <carllos.nc@gmail.com>
2020-12-02 17:36:55 -0300

    Refact: removing splash screen

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
 import React from "react"
 import { Routing } from "./config"
-import { Splash } from "./components"
 
 function App () {
   return (
     <>
-      <Splash />
       <Routing />
     </>
   )
